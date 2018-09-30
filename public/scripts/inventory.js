4f382b330d7ff7e941ef4e25b7a2502e10d28852
Jonathan Head <jonhead7240@gmail.com>
2018-09-30 10:03:38 -0500

    More work on the inventory system. Items now correctly display their quantity and icon image. Fixed a bug where releasing the mouse over a non-inventory slot would result in the movingFromSlot id not changing for the next item move.

--- public/scripts/inventory.js
+++ public/scripts/inventory.js
@@ -24,8 +24,18 @@ export default class Inventory{
     }
     init(){
         console.log('setting up inv!');
+        //this handles a bug where releasing the mouse over a non-inventory slot results in the movingFromSlot
+        //id not changing when attempting to move another item
+        window.document.addEventListener('mouseup',()=>{
+            if(this.movingItem){
+                this.movingItem = false;
+                this.movingFromSlot = -1;
+            }
+        });
         this.inventorySlots[0].slot.setItemId(1);
         this.inventorySlots[0].slot.setAmt(1000);
+        this.inventorySlots[4].slot.setItemId(2);
+        this.inventorySlots[4].slot.setAmt(20);
         this.slotListeners();
         this.update();
     }
@@ -37,22 +47,33 @@ export default class Inventory{
                 if(!this.movingItem){
                     this.movingItem = true;
                     this.movingFromSlot = target.slot.getSlotId();
+                    console.log(`Moving from slot: ${this.movingFromSlot}`);
                 }
             });
             target.docSlotTarget.addEventListener('mouseup',(event)=>{
                 console.log(`Mouse up on slot: ${target.slot.getSlotId()}`);
                 if(this.movingItem){
-                    this.movingToSlot = target.getSlotId();
+                    this.movingToSlot = target.slot.getSlotId();
+                    console.log(`Moving to slot: ${this.movingToSlot}`);
                     this.moveItem()
+                    this.movingItem = false;
+                    this.update();
                 }
             });
         }
     }
     moveItem(){
-        let target = this.inventorySlots;
-        
-        if(this.movingFromSlot != -1 && this.movingToSlot != -1 && this.movingFromSlot !== this.movingToSlot){
+        let targets = this.inventorySlots;
 
+        if(this.movingFromSlot != -1 && this.movingToSlot != -1 && this.movingFromSlot !== this.movingToSlot){
+            let slotAItemId = targets[this.movingFromSlot].slot.getItemId();
+            let slotAItemAmt = targets[this.movingFromSlot].slot.getAmt();
+            let slotBItemId  = targets[this.movingToSlot].slot.getItemId();
+            let slotBItemAmt = targets[this.movingToSlot].slot.getAmt();
+            targets[this.movingFromSlot].slot.setItemId(slotBItemId);
+            targets[this.movingFromSlot].slot.setAmt(slotBItemAmt);
+            targets[this.movingToSlot].slot.setItemId(slotAItemId);
+            targets[this.movingToSlot].slot.setAmt(slotAItemAmt);
         }
     }
     update(){
@@ -61,8 +82,10 @@ export default class Inventory{
            let amt = targetSlot.slot.getAmt();
 
 
-           if(amt>0){
-            targetSlot.slotAmtTarget.textContent = amt;
+           if(targetSlot.slot.getItemId() !== 0){
+               targetSlot.slotAmtTarget.textContent = amt;
+           }else{
+               targetSlot.slotAmtTarget.textContent = '';
            }
            let itemId = targetSlot.slot.getItemId();
            let imgPath = this.itemMap.getImgPathOfId(itemId);
